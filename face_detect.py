148976c208d509f2f167981b2cabdb7f3cc7520e
Ashish Nagar <ashishnagar31@gmail.com>
2018-03-03 22:08:56 +0530

    Adding argument passing

--- face_detect.py
+++ face_detect.py
@@ -8,20 +8,27 @@ import face_recognition
 from face_recognition import face_locations
 from face_recognition.cli import image_files_in_folder
 import cv2
+import argparse
 import time
+import sys
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 class FaceRecognizer():
-    def __init__(self, training_dir = None, model_path = ""):
+    def __init__(self, training_dir = None, model_path = "", test_image_store_location = None):
         self._model_path = model_path
         self._training_dir = training_dir
         self._predictor = None
         self._temp_file = 'temp.jpg'
         self._image_data = None
-        self._test_image_store_location = r""
+        self._test_image_store_location = test_image_store_location
 
+    
+    def exit_program(self, message, error_code):
+        print(message)
+        print('Aborting Program')
+        sys.exit(error_code)
     
     def train(self, n_neighbors = None, knn_algo = 'ball_tree', 
             image_face_only = False, sample_size = 200, image_store = 'pickled_images.model', load_saved_images = False, delete_missing_files = True):
@@ -32,8 +39,15 @@ class FaceRecognizer():
         train_dir = self._training_dir
         model_save_path = self._model_path
         if load_saved_images:
-            with open(image_store, 'rb') as f:
-                X, y, traversed_images = pickle.load(f)
+            if path.exists(image_store):
+                with open(image_store, 'rb') as f:
+                    X, y, traversed_images = pickle.load(f)
+            else:
+                self.exit_program("Image store not found...", 1)
+        else:
+            if path.exists(image_store):
+                self.exit_program("Please remove the existing image store and run the program again. Check in place to prevent accidental image store overwrites", 1)
+
 
         for class_dir in listdir(train_dir):
             print("Training for %s" % class_dir)
@@ -126,9 +140,6 @@ class FaceRecognizer():
             test_file_full_path = join(self._test_image_store_location, test_file_name)
             if not path.exists(directory):
                 makedirs(directory)
-            print("Directory : %s" % directory)
-            print("Test File : %s" %  test_file_full_path)
-            print('')
             cv2.imwrite(test_file_full_path, read[ b[0]:b[2], b[3]:b[1]] )
 
         return result
@@ -153,24 +164,29 @@ class FaceRecognizer():
                                     file_suffix = video_filename + '_frame_' + str(int(frame)) )
            
             if len(preds) > 0:
-                print(preds)
+                #print(preds)
                 if save_test_frame:
                     for pred in preds:
                         pred_name = 'unknown' if pred[0] == 'N/A' else pred[0]
+                        print("Face detected : %s" % pred_name)
                         face_loc = pred[1]
                         test_file_name = pred_name + '_' + video_filename + '_frame_' + str(int(frame)) + '.jpg'
                         directory = path.join(self._test_image_store_location, pred_name)
                         test_file_full_path = join(self._test_image_store_location,pred_name, test_file_name)
                         if not path.exists(directory):
                             makedirs(directory)
-                        print("Directory : %s" % directory)
-                        print("Test File : %s" %  test_file_full_path)
-                        print('')
+                        #print("Directory : %s" % directory)
+                        #print("Test File : %s" %  test_file_full_path)
+                        #print('')
                         cv2.imwrite(test_file_full_path, read[ face_loc[0]:face_loc[2], face_loc[3]:face_loc[1]] )
+
+                        if highlight_face:
+                            cv2.putText(read, pred_name, (face_loc[3], face_loc[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
+                            cv2.rectangle(read, (face_loc[3],face_loc[0]), (face_loc[1],face_loc[2]),(0,255,0),2)
 
                     
             if show_video:
-                res = cv2.resize(read,None,fx=0.2, fy=0.2, interpolation = cv2.INTER_CUBIC)
+                res = cv2.resize(read,None,fx=1.0, fy=1.0, interpolation = cv2.INTER_CUBIC)
                 cv2.imshow("Video", res)
             if cv2.waitKey(1) != -1:
                 vc.release()
@@ -179,9 +195,22 @@ class FaceRecognizer():
 
 
 if __name__ == '__main__':
-    ob = FaceRecognizer(training_dir = r'', model_path = 'new_recog.model')
+    parser = argparse.ArgumentParser(description = 'Arguments for Face Recognition')
+    parser.add_argument('--train_dir', action = 'store', dest = 'train_dir', required = True)
+    parser.add_argument('--video_path', action = 'store', dest = 'video_path', required = True)
+    parser.add_argument('--test_dir', action = 'store', dest = 'test_dir', required = True)
+    parser.add_argument('--image_store', action = 'store', dest = 'image_store', required = False)
+    parser.add_argument('--model_path', action = 'store', dest = 'model_path', required = False)
+    face_args = parser.parse_args()
+    train_dir = face_args.train_dir
+    video_path = face_args.video_path
+    test_dir = face_args.test_dir
+    image_store = face_args.image_store
+    model_path = face_args.model_path
+
+    ob = FaceRecognizer(training_dir = train_dir, test_image_store_location = test_dir, model_path = 'new_recog.model')
     clf = ob.train(image_face_only = True, load_saved_images = True)
     ob.load_predictor(knn_clf = clf)
-    ob.predict_in_video('', show_video = False, frame_skip_number = 50.0, save_test_frame = True, highlight_face = True)
+    ob.predict_in_video(video_path, show_video = True, frame_skip_number = 10.0, save_test_frame = True, highlight_face = True)
     #preds = ob.predict(r"")
     #print(preds)
